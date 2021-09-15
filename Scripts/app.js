2e946a52d778a979f5c2cbaddf958c0110f18c31
Elvis Li <presley.us@gmail.com>
2021-09-15 14:06:55 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,17 @@
+//IIFE -- Immediately Invoked Function Expression
+"use strict";
+(function(){
+
+    function Start(){
+
+        let myVariable = 20;
+        console.log("my number is: " + myVariable);
+
+    }
+
+window.addEventListener("load", Start);
+
+})();
+
+
+
